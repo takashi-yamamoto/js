e2473e3333e42d454b2890db8df8b6802d59a2d0
takashi-yamamoto <r.giggss@gmail.com>
2017-05-10 15:03:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ $(function () {
                 return false;
             });
     });
+
+/*==================================================================
+ Window Resize Function
+==================================================================*/
+
+
+var timer = false;
+var x = 600;
+$(window).resize(function() {
+    if (timer !== false) {
+        clearTimeout(timer);
+    }
+    timer = setTimeout(function() {
+    var w = $(window).width();
+		console.log(w);
+		if (w <= x) {
+        $('#element').css({
+            'background-color': '#333'
+        });
+    } else {
+        $('#element').css({
+           'background-color': '#fff'
+        });
+    }
+	}, 200);
+});	
